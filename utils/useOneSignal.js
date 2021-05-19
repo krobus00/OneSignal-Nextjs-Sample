c54e8c4f9e2620e048fa4420860993a9ea7f6ce1
William Shepherd <iAmWillShepherd@users.noreply.github.com>
2021-05-19 16:29:20 -0500

    Provide callback that initializes OneSignal to useEffect hook

---
+++ utils/useOneSignal.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+const useOneSignal = () =>
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: "Your App ID",
+        notifyButton: {
+          enable: true,
+        },
+        allowLocalhostAsSecureOrigin: true,
+      });
+    });
+
+    return () => {
+      window.OneSignal = undefined;
+    };
+  }, []); // <-- run this effect once on mount
+
+export default useOneSignal;
