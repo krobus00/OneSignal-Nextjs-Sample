a02b0c5678db6971e8e8cba6e39c06b61674d7f8
William Shepherd <iAmWillShepherd@users.noreply.github.com>
2021-05-19 16:28:25 -0500

    Use OneSignal hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import useOneSignal from "../utils/useOneSignal";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useOneSignal();
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
